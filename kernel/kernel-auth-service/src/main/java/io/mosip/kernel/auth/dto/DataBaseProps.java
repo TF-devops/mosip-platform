cb288be56031f364817dd651d229d8bddc3b6da2
TF-devops <mosip-devops@technoforte.co.in>
2019-12-19 17:08:23 +0530

    updating 1.0.0

---
+++ kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/dto/DataBaseProps.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package io.mosip.kernel.auth.dto;
+
+import lombok.Data;
+
+/**
+ * @author Ramadurai Pandian
+ *
+ */
+@Data
+public class DataBaseProps {
+	private String url;
+	private String port;
+	private String username;
+	private String password;
+	private String schemas;
+	private String driverName;
+	private String commonName;
+	private String adminDN;
+	private String adminPassword;
+}
